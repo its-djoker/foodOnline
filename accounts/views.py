6c8cdd1986e91b290393aa53b73dea1525f34620
its-djoker <homestay74@gmail.com>
2022-09-09 15:05:05 +1200

    vendor and customer dashboard edits

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,8 @@ from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 
+from vendor.models import Vendor
+
 # Restrict the vendor from accessing the customer page
 
 
@@ -196,7 +198,6 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-    # vendor = Vendor.objects.get(user=request.user)
     # orders = Order.objects.filter(
     #     vendors__in=[vendor.id], is_ordered=True).order_by('created_at')
     # recent_orders = orders[:10]
