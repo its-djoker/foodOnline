f602ce6432683b7347b24ae84d5a880a1492502f
its-djoker <homestay74@gmail.com>
2022-09-10 14:44:17 +1200

    vendor profile form + validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,8 @@
 from socket import fromshare
 from tkinter import Widget
 from django import forms
-from .models import User
+from .models import User, UserProfile
+from accounts.validators import allow_only_images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -20,3 +21,28 @@ class UserForm(forms.ModelForm):
 
         if password != confirm_password:
             raise forms.ValidationError("Password does not match")
+
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(
+        attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(
+        attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country',
+                  'city', 'pin_code', 'latitude', 'longitude']
+
+        # fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'country',
+        #           'city', 'pin_code', 'latitude', 'longitude']
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
--- accounts/models.py
+++ accounts/models.py
@@ -96,18 +96,17 @@ class UserProfile(models.Model):
         upload_to='users/profile_pictures', blank=True, null=True)
     cover_photo = models.ImageField(
         upload_to='users/cover_photos', blank=True, null=True)
-    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
-    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
     country = models.CharField(max_length=20, blank=True, null=True)
     city = models.CharField(max_length=20, blank=True, null=True)
     pin_code = models.CharField(max_length=6, blank=True, null=True)
     latitude = models.CharField(max_length=20, blank=True, null=True)
-    longtitude = models.CharField(max_length=20, blank=True, null=True)
+    longitude = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.email
 
-    def full_address(self):
-        return f'{self.address_line_1}, {self.address_line_2}'
+    # def full_address(self):
+    #     return f'{self.address_line_1}, {self.address_line_2}'
