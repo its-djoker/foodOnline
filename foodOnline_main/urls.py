a92a9f3921ea5480e9037c0ebe83f514a6c686a0
its-djoker <homestay74@gmail.com>
2022-09-23 19:26:49 +1200

    cart functionaility

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -18,11 +18,14 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from marketplace import views as MarketplaceViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('accounts/', include('accounts.urls')),
-    path('vendor/', include('vendor.urls')),
+    path('', include('accounts.urls')),
+    path('marketplace/', include('marketplace.urls')),
+    path('cart/', MarketplaceViews.cart, name='cart'),
+    # path('vendor/', include('vendor.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from vendor.models import Vendor
+
+# from django.contrib.gis.geos import GEOSGeometry
+# # ``D`` is a shortcut for ``Distance``
+# from django.contrib.gis.measure import D
+# from django.contrib.gis.db.models.functions import Distance
+
 
 def home(request):
-    return render(request, 'home.html')
+    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
+    context = {
+        'vendors': vendors,
+    }
+    return render(request, 'home.html', context)
+
+
+# def home(request):
+#     if get_or_set_current_location(request) is not None:
+
+#         pnt = GEOSGeometry('POINT(%s %s)' % (get_or_set_current_location(request)))
+
+#         vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=1000))).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
+
+#         for v in vendors:
+#             v.kms = round(v.distance.km, 1)
+#     else:
+#         vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+#     context = {
+#         'vendors': vendors,
+#     }
+#     return render(request, 'home.html', context)
