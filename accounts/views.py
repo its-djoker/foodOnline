b9a1cd52ef209f6109749f6eb2dcb127a016235d
its-djoker <homestay74@gmail.com>
2022-10-21 13:53:10 +1300

    orders and order detail functionalities - customer/vendor

--- accounts/views.py
+++ accounts/views.py
@@ -206,23 +206,27 @@ def vendorDashboard(request):
     recent_orders = orders[:10]
 
     # current month's revenue
-    # current_month = datetime.datetime.now().month
-    # current_month_orders = orders.filter(vendors__in=[vendor.id], created_at__month=current_month)
-    # current_month_revenue = 0
-    # for i in current_month_orders:
-    #     current_month_revenue += i.get_total_by_vendor()['grand_total']
+    current_month = datetime.datetime.now().month
+    current_month_orders = orders.filter(
+        vendors__in=[vendor.id], created_at__month=current_month)
+    current_month_revenue = 0
+    for i in current_month_orders:
+        current_month_revenue += i.get_total_by_vendor()['grand_total']
 
-    # # total revenue
-    # total_revenue = 0
-    # for i in orders:
-    #     total_revenue += i.get_total_by_vendor()['grand_total']
+    # total revenue
+    total_revenue = 0
+    for i in orders:
+        total_revenue += i.get_total_by_vendor()['grand_total']
     context = {
         'orders': orders,
         'orders_count': orders.count(),
         'recent_orders': recent_orders,
-        # 'total_revenue': total_revenue,
-        # 'current_month_revenue': current_month_revenue,
+        'total_revenue': total_revenue,
+        'current_month_revenue': current_month_revenue,
     }
+    print(orders)
+    print(orders.count())
+    print(recent_orders)
     return render(request, 'accounts/vendorDashboard.html', context)
 
 
--- vendor/urls.py
+++ vendor/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
 
     path('order_detail/<int:order_number>/',
          views.order_detail, name='vendor_order_detail'),
-    # path('my_orders/', views.my_orders, name='vendor_my_orders'),
+    path('my_orders/', views.my_orders, name='vendor_my_orders'),
 
 
 ]
